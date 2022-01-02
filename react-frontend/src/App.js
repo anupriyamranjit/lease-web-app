24d6dcaf090582bb59bf5dc3c4cf35c3ef65056f
Shaili Kadakia <kadsk61@gmail.com>
2022-01-02 12:04:54 -0500

    fixed logo issues

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
@@ -6,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
