183478dc9fbf7f6f4716c09f3b1d173bc008e276
Shaili Kadakia <kadsk61@gmail.com>
2022-01-06 21:30:33 -0500

    update package.json

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,22 +1,11 @@
 import './App.css';
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Map Leaflet</h1>
     </div>
   );
 }
