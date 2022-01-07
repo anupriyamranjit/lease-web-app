425ae23cd2959608081bdb823f32e109d2da5ba5
Shaili Kadakia <kadsk61@gmail.com>
2022-01-06 21:42:04 -0500

    added map code

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -4,9 +4,17 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Map Leaflet</h1>
-    </div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[51.505, -0.09]}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
   );
 }
 
