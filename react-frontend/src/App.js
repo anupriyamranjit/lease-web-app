9d3e3fc051b085f2f24fb65a0da225e1b29c2810
Shaili Kadakia <kadsk61@gmail.com>
2022-01-12 21:19:09 -0500

    tesla map

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,58 +1,46 @@
-import React, {useState, useEffect, useRef} from "react";
+import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './App.css';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-
-import nationalParks from './national-parks.json';
-
+import teslaData from "./data/tesla-sites.json"
 
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
+
+const filteredStations = teslaData.filter(tesla => tesla.address.country === "Italy")
 function App() {
-  const mapRef = useRef();
-
-  useEffect(() => {
-    const { current = {} } = mapRef;
-    const { leafletElement: map } = current;
-
-    if ( !map ) return;
-
-  
-    const parksGeoJson = new L.GeoJSON(nationalParks, {
-      onEachFeature: (feature = {}, layer) => {
-        const { properties = {} } = feature;
-        const { Name } = properties;
-
-        if ( !Name ) return;
-
-        layer.bindPopup(`<p>${Name}</p>`);
-      }
-    });
-    
-
-    parksGeoJson.addTo(map);
-  }, [])
+  console.log(teslaData);
 
   return (
-    <div className="App">
-      <MapContainer ref={mapRef} center={[39.50, -98.35]} zoom={4}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
-        <Marker position={[50.131129569256473, -100.325508673715092]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-      </MapContainer>
-    </div>
+    <MapContainer center={[42.58544, 13.257684]} zoom={6} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {filteredStations.map(tesla => (
+        <Marker 
+        key = {tesla.id}
+        position={[ tesla.gps.latitude, tesla.gps.longitude]}>
+
+          <Popup position={[ tesla.gps.latitude, tesla.gps.longitude]}>
+            <div>
+              <h2>{"Name:" + tesla.name}</h2>
+              <p>{"Status:" + tesla.status}</p>
+            </div>
+
+          </Popup>
+
+        </Marker>))}
+    </MapContainer>
   );
 }
-
 
 export default App;
