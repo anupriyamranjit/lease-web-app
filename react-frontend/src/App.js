ea37c6e72eeeaa5064456e703ea12981e5e9ed94
Anupriyam Ranjit <anupriyamranjit@Anupriyams-MacBook-Air.local>
2022-01-28 21:42:05 -0500

    Finished setting up Filter component need to do a bunch of styling and add more filter

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,22 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Map from "../src/Components/Map/Map.js";
 import SearchBar from './Components/SearchBar/SearchBar';
 import Data from "./data/tesla-sites.json";
+import { FilterComponent } from './Components/Filters/Filters';
+import { Box } from '@mui/system';
 
-class App extends Component {
-  render() {
+function App() {
     return (
       <div className='App'>
         <div className='Header'>
           <SearchBar data={Data} />
         </div>
-        <div className='Content'>
-          <Map></Map>
+        <div className='Filter'>
+          { false && <FilterComponent data={Data} /> }
         </div>
-      </div>
+        <div className='Content'>
+          <Box width={700}>
+          <Map />
+          </Box>
+        </div>
+        </div>
     );
   }
-}
 
 export default App;
