78be5515ae700585f3afa8d978930c3a0f3c3273
Neel Shah <neelshahh14@gmail.com>
2022-03-06 12:07:14 -0500

    added test uri and cross-env

--- node-server/database/database.js
+++ node-server/database/database.js
@@ -6,11 +6,18 @@ class Database {
         this._connect()
     }
     _connect() {
-        const uri = process.env.ATLAS_URI;
-        const connectionParams = {useNewUrlParser: true, useUnifiedTopology: true};
+        const env = process.env;
+        let uri;
+        if (env.NODE_ENV === "test") {
+            uri = env.TEST_URI;
+        } else {
+            uri = env.ATLAS_URI;
+        }
+
+        const connectionParams = { useNewUrlParser: true, useUnifiedTopology: true };
         mongoose.connect(uri, connectionParams)
-        .then(() => console.log("Connection Successful"))
-        .catch(err => console.error(err));
+            .then(() => console.log("Connection Successful"))
+            .catch(err => console.error(err));
     }
 }
 
