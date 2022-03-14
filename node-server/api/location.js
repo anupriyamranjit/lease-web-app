eacb287a5ccb54bb89abe29e6636c70a16e8b069
Neel Shah <neelshahh14@gmail.com>
2022-03-14 12:26:57 -0400

    moved around some code to fix merge conflicts

--- node-server/api/location.js
+++ node-server/api/location.js
@@ -4,74 +4,134 @@ const router = express.Router();
 
 // GET Route: All Locations
 router.route('/').get(async (req, res) => {
+    const { page = 1, limit = 10 } = req.query;
     try {
-        await Location.create({
-            name: 'CMH',
-            description: 'Waterloo Residence Builing',
-            numberOfBathrooms: 0,
-            numberOfBedrooms:0,
-            address: {
-                streetNumber: 165,
-                streetName: 'University Avenue W',
-                city: 'Waterloo',
-                province: 'Ontario',
-                postalCode: 'N2L 3E8',
-                latitude: 43.49041374273173,
-                longtitude:  -80.54281045857041,
-            },
-            price: 450,
-            utilities: {
-                hydroIncluded: true,
-                hydroPrice: 45,
-                electricalIncluded: false,
-                electricalPrice: null,
-                laundryIncluded: true,
-                laundryPrice: 23,
-                internetIncluded: false,
-                internetPrice: null,
-                totalUtilitiesPrice: 68
-            },
-            other: {
-                hasGym: true,
-                hasBikeRake: true,
-                hasParking: false,
-                parkingPrice: 0,
-                furnitureIncluded: true,
-                other: 'Hi, welcome to CMH'
-            }
-        })
-        const location = await Location.find();
+        const location = await Location.find()
+            .limit(limit)
+            .skip((page - 1) * limit)
+            .exec()
         res.json(location);
-    } catch(e){
+    } catch (e) {
         res.status(400).json("Error: " + e);
     }
-  })
+})
 
 
-  // Patch Route: Single Location
+router.route('/:id').get(async (req, res) => {
+    const { id } = req.params
+    try {
+        const location = await Location.findById(id);
+        res.json(location);
+    } catch (e) {
+        res.status(404).json("Error: " + e);
+    }
+})
 
-router.route('/update/:id').patch(async (req, res ) => {
+// POST Route: Creates New Location
+router.route('/addLocation').post(async (req, res) => {
     try {
         // Constants
         const {
             name, description, numberOfBathrooms, numberOfBedrooms,
-            address: { 
-                streetNumber, streetName, city, province, postalCode, latitude, longtitude 
+            address: { streetNumber, streetName, city, province, postalCode, latitude, longtitude },
+            price,
+            utilities: { hydroIncluded, hydroPrice, electricalIncluded, electricalPrice, laundryIncluded,
+                laundryPrice, internetIncluded, internetPrice, totalUtilitiesPrice },
+            other: { hasGym, hasBikeRake, hasParking, parkingPrice, furnitureIncluded, other }
+        } = req.body;
+
+        // Variables
+        let findLocation;
+        let newLocation;
+
+        // Create a new location if a location with the same name does not exist
+        findLocation = await Location.find({ "name": name });
+        if (findLocation.length === 0) {
+            newLocation = new Location({
+                name: name,
+                description: description,
+                numberOfBathrooms: numberOfBathrooms,
+                numberOfBedrooms: numberOfBedrooms,
+                address: {
+                    streetNumber: streetNumber,
+                    streetName: streetName,
+                    city: city,
+                    province: province,
+                    postalCode: postalCode,
+                    latitude: latitude,
+                    longtitude: longtitude,
+                },
+                price: price,
+                utilities: {
+                    hydroIncluded: hydroIncluded,
+                    hydroPrice: hydroPrice,
+                    electricalIncluded: electricalIncluded,
+                    electricalPrice: electricalPrice,
+                    laundryIncluded: laundryIncluded,
+                    laundryPrice: laundryPrice,
+                    internetIncluded: internetIncluded,
+                    internetPrice: internetPrice,
+                    totalUtilitiesPrice: totalUtilitiesPrice
+                },
+                other: {
+                    hasGym: hasGym,
+                    hasBikeRake: hasBikeRake,
+                    hasParking: hasParking,
+                    parkingPrice: parkingPrice,
+                    furnitureIncluded: furnitureIncluded,
+                    other: other
+                }
+            });
+            await newLocation.save();
+            res.json('Location with name: ${name} was added!')
+        } else {
+            // should we be doing something else here?
+            throw "A location with the same name already exists!";
+        }
+    } catch (e) {
+        res.status(400).json('Error: ' + e);
+    }
+})
+
+// DELETE Route: Delete Location
+router.route('/:id').delete(async (req, res) => {
+    try {
+        // Constants
+        const { id } = req.params;
+
+        // Find location by id and delete from db
+        foundLocation = await Location.findById(id);
+        await foundLocation.remove();
+        res.json(`Location with id ${id} was deleted!`);
+
+    } catch (e) {
+        res.status(400).json('Error: ' + e);
+    }
+})
+
+// Patch Route: Single Location
+router.route('/update/:id').patch(async (req, res) => {
+    try {
+        // Constants
+        const {
+            name, description, numberOfBathrooms, numberOfBedrooms,
+            address: {
+                streetNumber, streetName, city, province, postalCode, latitude, longtitude
             },
             price,
-            utilities: { 
+            utilities: {
                 hydroIncluded, hydroPrice, electricalIncluded, electricalPrice, laundryIncluded,
-                laundryPrice, internetIncluded, internetPrice, totalUtilitiesPrice 
+                laundryPrice, internetIncluded, internetPrice, totalUtilitiesPrice
             },
-            other: { 
+            other: {
                 hasGym, hasBikeRake, hasParking, parkingPrice, furnitureIncluded, other
-             }
+            }
         } = req.body;
-      
+
         // Variables 
         let foundLocation = await Location.findById(id);
 
-        if ( foundLocation != null ) {
+        if (foundLocation != null) {
             console.log("Error: Location ID does not exist")
         } else {
             console.log("Location ID was found")
@@ -105,10 +165,11 @@ router.route('/update/:id').patch(async (req, res ) => {
             foundLocation.other.other = other;
             await foundLocation.save();
             console.log(`Location with id ${id} is updated`);
-        } 
+        }
     } catch (e) {
         res.status(400).json("Error: " + e);
     }
 });
+
 
 module.exports = router
