6d5d34398141f508876311edcefdfa068420f319
Anupriyam Ranjit <anupriyamranjit@Anupriyams-MacBook-Air.local>
2022-03-18 19:41:00 -0400

    Formatted code

--- node-server/model/location.model.js
+++ node-server/model/location.model.js
@@ -1,42 +1,119 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    name: { type: String, trim: true },
-    description: { type: String },
-    numberOfBathrooms: { type: Number, required: true, min: 0 },
-    numberOfBedrooms: { type: Number, required: true, min: 0 },
-    address: {
-      streetNumber: { type: Number, required: true, min: 0 },
-      streetName: { type: String, required: true },
-      city: { type: String, required: true },
-      province: { type: String, required: true },
-      postalCode: { type: String, required: true },
-      latitude: { type: Number, required: true },
-      longitude: { type: Number, required: true },
+  name: {
+    type: String,
+    trim: true
+  },
+  description: {
+    type: String
+  },
+  numberOfBathrooms: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  numberOfBedrooms: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  address: {
+    streetNumber: {
+      type: Number,
+      required: true,
+      min: 0
     },
-    price: { type: Number, required: true, min: 0 },
-    utilities: {
-      hydroIncluded: { type: Boolean },
-      hydroPrice: { type: Number, min: 0 },
-      electricalIncluded: { type: Boolean },
-      electricalPrice: { type: Number, min: 0 },
-      laundryIncluded: { type: Boolean },
-      laundryPrice: { type: Number, min: 0 },
-      internetIncluded: { type: Boolean },
-      internetPrice: { type: Number, min: 0 },
-      totalUtilitiesPrice: { type: Number, min: 0 },
+    streetName: {
+      type: String,
+      required: true
+    },
+    city: {
+      type: String,
+      required: true
+    },
+    province: {
+      type: String,
+      required: true
+    },
+    postalCode: {
+      type: String,
+      required: true
+    },
+    latitude: {
+      type: Number,
+      required: true
+    },
+    longitude: {
+      type: Number,
+      required: true
+    },
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  utilities: {
+    hydroIncluded: {
+      type: Boolean
+    },
+    hydroPrice: {
+      type: Number,
+      min: 0
+    },
+    electricalIncluded: {
+      type: Boolean
+    },
+    electricalPrice: {
+      type: Number,
+      min: 0
+    },
+    laundryIncluded: {
+      type: Boolean
+    },
+    laundryPrice: {
+      type: Number,
+      min: 0
+    },
+    internetIncluded: {
+      type: Boolean
+    },
+    internetPrice: {
+      type: Number,
+      min: 0
+    },
+    totalUtilitiesPrice: {
+      type: Number,
+      min: 0
+    },
+  },
+  other: {
+    hasGym: {
+      type: Boolean
+    },
+    hasBikeRake: {
+      type: Boolean
+    },
+    hasParking: {
+      type: Boolean
+    },
+    parkingPrice: {
+      type: Number,
+      min: 0
+    },
+    furnitureIncluded: {
+      type: Boolean
     },
     other: {
-      hasGym: { type: Boolean },
-      hasBikeRake: { type: Boolean },
-      hasParking: { type: Boolean },
-      parkingPrice: { type: Number, min: 0 },
-      furnitureIncluded: { type: Boolean },
-      other: { type: String },
+      type: String
     },
-    isSublease: { type: Boolean }
-  })
+  },
+  isSublease: {
+    type: Boolean
+  },
+});
 
-const Location = mongoose.model('Location', schema);
+const Location = mongoose.model("Location", schema);
 
-module.exports = Location
+module.exports = Location;
