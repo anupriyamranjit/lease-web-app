f6de14834dbfb276e26f480f90ad4917228bc6af
Anupriyam Ranjit <anupriyamranjit@Anupriyams-MacBook-Air.local>
2022-03-18 18:29:57 -0400

    Removed test file

--- node-server/model/test.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose")
-
-const schema = new mongoose.Schema({ name: 'string', size: 'string' });
-const Test = mongoose.model('Test', schema);
-
-module.exports = Test
